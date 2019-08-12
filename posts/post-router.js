e8ca8b4dded8dc0a3fedf69a9f5795a7f4b9cf8b
Reed James <reedtjames1234@gmail.com>
2019-08-12 13:22:14 -0500

    Demo finished again

--- posts/post-router.js
+++ posts/post-router.js
@@ -48,13 +48,33 @@ router.put("/:id", (req, res) => {
     .where("id", "=", req.params.id)
     .update({ title: "new title" })
     .then(count => {
-      res.status(200).json(count);
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: "not found" });
+      }
     })
     .catch(err => {
       res.status(500).json({ message: "bummer" });
     });
 });
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  db("posts")
+    .where("id", "=", req.params.id)
+    .delete()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: "not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "bummer" });
+    });
+});
+
+// 15580, 15987
 
 module.exports = router;
