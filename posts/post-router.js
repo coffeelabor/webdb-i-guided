a0d7ad0f6f96dcbcb15ead9bf9a32bab2b137f88
Reed James <reedtjames1234@gmail.com>
2019-08-12 13:21:29 -0500

    Demo finished

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,60 @@
-const express = require('express');
+const express = require("express");
 
 // database access using knex
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get("/", (req, res) => {
+  // db('posts') //returns a promise that resolves to all records from the posts table
+  db.select("id", "title", "contents")
+    .from("posts")
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "bummer" });
+    });
 });
 
-router.get('/:id', (req, res) => {
-
+router.get("/:id", (req, res) => {
+  db("posts")
+    .where({ id: req.params.id })
+    .first()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "bummer" });
+    });
 });
 
-router.post('/', (req, res) => {
-
+router.post("/", (req, res) => {
+  const post = req.body;
+  //validate that the post data is correct before saving to the db
+  db("posts")
+    .insert(post, "id")
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "bummer" });
+    });
 });
 
-router.put('/:id', (req, res) => {
-
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  db("posts")
+    .where("id", "=", req.params.id)
+    .update({ title: "new title" })
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "bummer" });
+    });
 });
 
-router.delete('/:id', (req, res) => {
-
-});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
